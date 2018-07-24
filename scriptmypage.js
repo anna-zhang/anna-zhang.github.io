33a26828dfaaef3dd75918d065e514726bd94d47
unknown <annazhangg@yahoo.com>
2018-07-24 15:25:34 -0400

    First commit!

---
+++ scriptmypage.js
@@ -0,0 +1,10 @@
+// alert("I just wrote JavaScript code!")
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+    
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
