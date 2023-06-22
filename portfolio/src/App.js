847f380ab0e63bc87cc435488511cda48f75e982
Anna Zhang <31214438+anna-zhang@users.noreply.github.com>
2023-06-22 08:47:12 -0400

    react portfolio setup

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App () {
+  return (
+    <nav className='App'>
+      <h1>
+        <a href='index'>Anna Zhang</a>
+      </h1>
+      <a href='info'>Info</a>
+    </nav>
+  )
+}
+
+export default App
